b5a0c45b19ca959c99dd01550a73ae9d187954ef
George Kankava <george.kankava@aurea.com>
2016-02-05 17:43:39 +0400

    squid:UselessParenthesesCheck - Useless parentheses around expressions should be removed to prevent any misunderstanding

--- json/src/main/java/org/openx/data/jsonserde/json/JSONObject.java
+++ json/src/main/java/org/openx/data/jsonserde/json/JSONObject.java
@@ -965,7 +965,7 @@ public class JSONObject {
 
         boolean includeSuperClass = klass.getClassLoader() != null;
 
-        Method[] methods = (includeSuperClass) ?
+        Method[] methods = includeSuperClass ?
                 klass.getMethods() : klass.getDeclaredMethods();
         for (int i = 0; i < methods.length; i += 1) {
             try {
